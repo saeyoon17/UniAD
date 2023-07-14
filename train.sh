6936463a0233bc9f1cbd14813a909a7164ff57af
Saeyoon Oh <43169204+saeyoon17@users.noreply.github.com>
2023-07-14 10:40:34 +0900

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+ source /root/.bashrc
  conda activate uniad
  pip install torch==1.9.1+cu111 torchvision==0.10.1+cu111 torchaudio==0.9.1 -f https://download.pytorch.org/whl/torch
  export PATH=/usr/bin/gcc/bin:$PATH
